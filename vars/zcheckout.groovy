8a6512f3baeff9709cde1621723d4d6b58448395
Nikita Blintsov <nikita.blintsov@wayray.com>
2019-01-24 09:08:13 +0300

    fix commit

--- vars/zcheckout.groovy
+++ vars/zcheckout.groovy
@@ -1,38 +1,39 @@
 #!/usr/bin/groovy
 
 def call(String source = '', String target = '') {
-    checkout(
-        changelog: true,
-        poll: true,
-        scm: [
-            $class: 'GitSCM',
-            branches: [
-                [
-                    name: "origin/${source}"
-                ]
-            ],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-                [
-                    $class: 'WipeWorkspace'
-                ],
-                [
-                    $class: 'CleanBeforeCheckout'
-                ],
-                [
-                    $class: 'LocalBranch',
-                    localBranch: 'jenkins/master'
-                ],
-                [
-                    $class: 'PreBuildMerge',
-                    options: [
-                        fastForwardMode: 'FF',
-                        mergeRemote: 'origin',
-                        mergeTarget: '${target}'
-                    ]
-                ]
-            ],
-            submoduleCfg: [],
-        ]
-    )
+    sh "echo ${source} and ${target}"
+//    checkout(
+//        changelog: true,
+//        poll: true,
+//        scm: [
+//            $class: 'GitSCM',
+//            branches: [
+//                [
+//                    name: "origin/${source}"
+//                ]
+//            ],
+//            doGenerateSubmoduleConfigurations: false,
+//            extensions: [
+//                [
+//                    $class: 'WipeWorkspace'
+//                ],
+//                [
+//                    $class: 'CleanBeforeCheckout'
+//                ],
+//                [
+//                    $class: 'LocalBranch',
+//                    localBranch: 'jenkins/master'
+//                ],
+//                [
+//                    $class: 'PreBuildMerge',
+//                    options: [
+//                        fastForwardMode: 'FF',
+//                        mergeRemote: 'origin',
+//                        mergeTarget: '${target}'
+//                    ]
+//                ]
+//            ],
+//            submoduleCfg: [],
+//        ]
+//    )
 }
